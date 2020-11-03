f30c5571ca1f6fd310e90c0c56e60f871333c65f
joriankoning <jjkoning@st.noorderpoort.nl>
2020-11-03 16:35:00 +0100

    Een klein begin

---
+++ hardware.h
@@ -0,0 +1,17 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 3-11-2020 16:20:21
+ *  Author: joria
+ */ 
+
+#define HIGH 0x1;
+#define LOW 0x0;
+
+#define rood = PORTD4;			// rode led
+#define groen = PORTD5;			// groene led
+#define geel = PORTD6;			// gele led
+#define blauw = PORTD7;			// blauwe led
+
+#define InUit = PIND3;			// knopje om scherm in/uit te doen
+#define automaat = PIND2;		// knopje om scherm automatisch in/uit te doen
