729c51305a5fd5c6fe7791b6f5f716004432bf3b
jasper-geertsma <58548553+jasper-geertsma@users.noreply.github.com>
2020-11-10 11:45:31 +0100

    commentaar toegevoegd

--- txtwrite.py
+++ txtwrite.py
@@ -3,12 +3,13 @@ import time
 
 #Open com, zet goeie com hier neer
 ser = serial.Serial('COM6', 19200)
-
+#maakt een waarde voor de x as
 i=0
 
 print(ser)
 time.sleep(4)
 t=0
+
 file1 = open("Temperatuur.txt", "a")
 file1.truncate(0)# Zorgt ervoor dat Temperatuur.txt leeg wordt
 file1.close()
@@ -19,11 +20,11 @@ file2.close()
 
 
 while True:
-
+    #haalt een waarde op van de arduino
     firstBitcheck = ser.read()
     j = firstBitcheck.hex()
     checker = int(j, 16)
-    
+    #controlleert of de volgende waaarde een 1 is zodat het programma weet dat er een nieuwe loop is
     if checker == 1:
         time.sleep(1)
         tempRead = ser.read()
@@ -48,7 +49,8 @@ while True:
     elif checker == 2:
         time.sleep(1)
         lichtRead = ser.read()
-        
+
+        #schrijft naar licht.txt
         file2 = open("Licht.txt", "a")
         licht = int(lichtRead.hex(), 16)
         file2.write(str(t)+ "," + str(licht) + "\n")
@@ -58,7 +60,7 @@ while True:
         
     else:
         time.sleep(1)
-	
+	#leest uit afstand.txt zodat er geschreven kan worden naar de arduino
     pullData = open("Afstand.txt", "a")
     file = open("Afstand.txt", "r").read()
     if file != "":
@@ -66,5 +68,5 @@ while True:
         ser.write(data)
         pullData.truncate(0)
         print(data)
-	
+
     t += 1
